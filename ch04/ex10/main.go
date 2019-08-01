a0f725cd1b4e3befef7fa58e765333b114698dbb
stokuda <tokuda.sss@gmail.com>
2019-08-02 00:26:40 +0900

    mod

--- ch04/ex10/main.go
+++ ch04/ex10/main.go
@@ -4,7 +4,18 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
+	"time"
 )
+
+const (
+	secInOneMonth = 60 * 60 * 24 * 30
+	secInOneYear  = secInOneMonth * 12
+)
+
+func l(item *Issue) {
+	fmt.Printf("#%-5d %s %9.9s %.55s\n", item.Number, item.CreatedAt.Format("2006-01-03"), item.User.Login, item.Title)
+}
 
 func main() {
 	result, err := SearchIssues(os.Args[1:])
@@ -12,8 +23,35 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Printf("%d issues:\n", result.TotalCount)
-	for _, item := range result.Items {
-		fmt.Printf("#%-5d %9.9s %.55s\n",
-			item.Number, item.User.Login, item.Title)
+
+	items := result.Items
+	sort.Slice(items, func(i, j int) bool {
+		return items[i].CreatedAt.After(items[j].CreatedAt)
+	})
+
+	printOneMonthBanner := true
+	printOneYearBanner := true
+	printOtherBanner := true
+
+	for _, i := range items {
+
+		if i.CreatedAt.AddDate(0, 1, 0).After(time.Now()) {
+			if printOneMonthBanner {
+				fmt.Println("\n==== now - createdAt < oneMonth ====")
+				printOneMonthBanner = false
+			}
+		} else if i.CreatedAt.AddDate(1, 0, 0).After(time.Now()) {
+			if printOneYearBanner {
+				fmt.Println("\n==== now - createdAt < oneYear ====")
+				printOneYearBanner = false
+			}
+		} else {
+			if printOtherBanner {
+				fmt.Println("\n==== now - createdAt > oneYear ====")
+				printOtherBanner = false
+			}
+		}
+		l(i)
+
 	}
 }
---
+++ ch04/ex10/run.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+go build && ./ex10 gorm
